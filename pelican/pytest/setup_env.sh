ddfc679c2fc29f11edf1202fbad3b32dc9784a8b
Brandon Bagby <brandonbagby@Brandons-MacBook-Air.local>
2025-07-19 02:41:48 -0400

    Finished Reader tests

--- pelican/pytest/setup_env.sh
+++ pelican/pytest/setup_env.sh
@@ -4,6 +4,7 @@ if [ ! -d .pytest_venv ]; then
     source .pytest_venv/bin/activate
     pip3 install pytest
     pip3 install pytest-html
+    pip3 install pelican
 else
     echo "\n"
     echo "VENV already exists"
--- pelican/pytest/test_readers.py
+++ pelican/pytest/test_readers.py
@@ -1,49 +1,62 @@
 import pytest
-from readers import HTMLReader
+from ..readers import HTMLReader
+from ..settings import DEFAULT_CONFIG
 
-class TestHTMLReader(HTMLReader):
 
-    class TestHTMLParser(self._HTMLParser):
-        
-        ids = ["T1", "T2",
-               "T3", "T4",
-               "T5", "T6",
-               "T7", "T8",
-               "T9"]
+build_tag_data = [
+    ("meta", [("name", "description")], True, '<meta name="description" />'),
+    ("body", [("name", None)], False, "<body name>"),
+    ("title", [], True, "<title />"),
+    ("", [("name", "description")], False, '< name="description">'),
+    ("", [("name", None)], True,  "< name />"),
+    ("", [], False, "<>"),
+    ("123", [("name", "de$cription")], True, '<123 name="de$cription" />'),
+    ("!!", [("name", None)], False, "<!! name>"),
+    ("$$$", None, True, TypeError)
+]
 
-        start_tag_data = [
-            ("meta", [("name", "description")], ""),
-            ("body", [("name", None)], ""),
-            ("title", [], ""),
-            ("", [("name", "description")], ""),
-            ("", [("name", None)], ""),
-            ("", [], ""),
-            ("123", [("name", "de$cription")], ""),
-            ("!!", [("name", None)], ""),
-            ("$$$", [], "")
-        ]
+read_data = [
+    ("./data/T1.html", ('TEST HTML', {})),
+    ("./data/T2.html", ('', {})),
+    ("./data/T3.html", FileNotFoundError),
+]
 
-        build_tag_data = [
-            ("meta", [("name", "description")], True, ""),
-            ("body", [("name", None)], False, ""),
-            ("title", [], True, ""),
-            ("", [("name", "description")], False, ""),
-            ("", [("name", None)], True,  ""),
-            ("", [], False, ""),
-            ("123", [("name", "de$cription")], True, ""),
-            ("!!", [("name", None)], False, ""),
-            ("$$$", [], True, "")
-        ]
 
-        pytest.mark.parametrize("tag, attrs, expected", start_tag_data, ids=ids)
-        def test_handle_start_tag(tag, attrs, expected):
-            """
-            """
-            pass
-    
-        pytest.mark.parametrize("tag, attrs, close_tag, expected", build_tag_data, ids=ids)
-        def test_build_tag(tag, attrs, close_tag, expected):
-            """
-            """
-            pass
+@pytest.fixture(name="html_reader")
+def html_reader_obj():
+    settings = DEFAULT_CONFIG
+    yield HTMLReader(settings)
 
+
+@pytest.fixture(name="html_parser")
+def html_parser_obj():
+    settings = DEFAULT_CONFIG
+    filename = ""
+    yield HTMLReader._HTMLParser(settings, filename)
+
+
+@pytest.mark.parametrize("tag, attrs, close_tag, expected", build_tag_data, ids=["T1", "T2", "T3", "T4",
+                                                                                 "T5", "T6", "T7", "T8", "T9"])
+def test_parser_build_tag(tag, attrs, close_tag, expected, html_parser):
+    """
+    Test HTMLReader.HTMLParser build tag functionality
+    """
+    try:
+        output = html_parser.build_tag(tag, attrs, close_tag)
+        assert output == expected
+    except TypeError:
+        # Error Case
+        assert expected == TypeError
+
+
+@pytest.mark.parametrize("filename, expected", read_data, ids=["T1", "T2", "T3"])
+def test_parser_read(filename, expected, html_reader):
+    """
+    Test HTMLReader read html functionality
+    """
+    try:
+        output = html_reader.read(filename)
+        assert output == expected
+    except FileNotFoundError:
+        # Error Case
+        assert expected == FileNotFoundError
